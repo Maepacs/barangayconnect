a41a408afb8e08fc57017d68f31f29f4c8a73662
Mae Ann Pacquiao <142951976+Maepacs@users.noreply.github.com>
2025-09-28 18:04:07 +0800

    add config

--- cons/config.php
+++ cons/config.php
@@ -1,1 +1,14 @@
-database
+<?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db   = "barangay_connect";
+
+// Create connection
+$conn = mysqli_connect($host, $user, $pass, $db);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+?>
